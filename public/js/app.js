3a4d8d09cd87158b975830e86187034deecc7b96
Roman <wfsdrp@gmail.com>
2021-09-23 20:09:56 +0300

    add MainController

---
+++ public/js/app.js
@@ -0,0 +1,3 @@
+$(document).on('change','#link',function (){
+    let link = $(this).val();
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,12 @@
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<?php
-if(DB::connection()->getDatabaseName())
-{
-echo "Yes! successfully connected to the DB: " . DB::connection()->getDatabaseName();
-}
-?>
-</body>
-</html>
+@extends('layout')
+
+@section('main_content')
+    <form method="get" action="/checkform" class="input-group has-validation">
+        @csrf
+        <span class="input-group-text">Ссылка</span>
+        <input type="url" class="form-control" name="link" id="link" placeholder="https://www.instagram.com/gushchinvv/" required="">
+        <div class="invalid-feedback">
+            Необходимо указать ссылку на ваш профиль.
+        </div>
+    </form>
+@endsection
