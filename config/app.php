feaf1d37f704721dfebc6bf960b5ec1c019f95f9
Roman <wfsdrp@gmail.com>
2021-09-23 22:08:02 +0300

    fix menu

--- config/app.php
+++ config/app.php
@@ -190,7 +190,6 @@ return [
     */
 
     'aliases' => [
-        'Agent' => 'Jenssegers\Agent\Facades\Agent::class',
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
